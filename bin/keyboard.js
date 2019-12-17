623422524e35e00422b34a06a8f149e59457b176
UserHyuser <drachevskii@yandex.ru>
2019-12-17 16:21:36 +0700

    Bot is working. Modules only for FAQ questions

---
+++ bin/keyboard.js
@@ -0,0 +1,30 @@
+const inline_keyboard = [
+	[
+		{
+			text: 'Вопрос #1',
+			callback_data:'1'
+		},
+		{
+			text: 'Вопрос #2',
+			callback_data:'2'
+		}
+	],
+	[
+		{
+			text: 'Вопрос #3',
+			callback_data:'3'
+		},
+		{
+			text: 'Вопрос #4',
+			callback_data:'4'
+		}
+	],
+	[
+		{
+			text: 'Следущие вопросы',
+			callback_data:'faq_next_question_1'
+		}
+	]
+];
+
+module.exports = inline_keyboard;
--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 const Telegraf = require('telegraf');
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 require('dotenv').config(); // Load config from .env file
-const SocksAgent = require('socks-proxy-agent');
+const HttpsProxyAgent = require('https-proxy-agent');
 
-console.log("Connecting to proxy...");
-let proxy = 'socks://' + process.env.PROXY;
-socksAgent = new SocksAgent(proxy, function () {
-	console.log("Proxy connected");
+// const keyboard = require('./bin/keyboard'); // Global access
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+	const chatId = msg.chat.id;
+
+	bot.sendMessage(chatId, `Здравствуйте! Это пример бота, который может быть ваш.
+	Введите /faq, чтобы увидеть пример FAQ`);
 });
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN,{
-	// telegram: {agent: socksAgent}
+bot.onText(/\/faq/, (msg) => {
+	const keyboard = require('./bin/keyboard');
+	const chatId = msg.chat.id;
+
+	bot.sendMessage(chatId, `#1. Как связаться с автором этого бота?
+#2. Можно ли мне заказать такого же бота?
+#3. А сколько это будет стоить?
+#4. А что по срокам?`, {
+		reply_markup:{
+			inline_keyboard: keyboard
+		}
+	});
 });
 
-bot.start((ctx) => ctx.reply('Welcome!'));
+bot.sendMessage(process.env.ADMIN_ID, "Бот запущен!");
 
-bot.launch();
-console.log("Bot started...")
+bot.on('callback_query', function (query) {
+	const {chat, message_id, text} = query.message;
+
+	fs.readdir("./faq", function (err, files) { // Count files in faq
+		let amount = files.length;
+	})
+
+	if(parseInt(query.data).toString() === query.data){
+		fs.readFile(`./faq/${query.data}.txt`, 'UTF-8', function (err, answer) {
+			if(err) console.log(err)
+			else {
+				answer += "\n\nОстальные вопросы: /faq";
+				bot.sendMessage(chat.id, answer)
+			}
+		})
+	} else{
+		bot.sendMessage(chat.id, process.env.ERR_MSG)
+	}
+
+	// Pick button as done
+	bot.answerCallbackQuery({
+		callback_query_id: query.id
+	})
+})
+
+bot.on('polling_error', (error) => {
+	console.log('POOLING ERROR')
+	console.log(error.code);  // => 'EFATAL'
+});
+
+console.log("Bot started...");
